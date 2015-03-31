090772ca485a6c5ff87a1e9179e15d40a3c1cc1a
William Tsang <williamkt91@gmail.com>
2015-03-31 16:16:35 -0700

    adding trip create action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,4 +12,21 @@ class TripsController < ApplicationController
     return render json: trip, status: 200
   end
 
+  def create
+    user = User.find_by(id: params[:user_id])
+    create_trip = Trip.create(
+      user_id: params[:user_id],
+      trip_name: params[:tripName],
+      location: params[:tripLocation],
+      startdate: params[:startdate],
+      enddate: params[:enddate]
+      )
+  end
+
+  private
+
+  def safe_params
+    
+  end
+
 end
