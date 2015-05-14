ac4f848c0cdbb324936eb388a0289026de791ebc
William Tsang <williamkt91@gmail.com>
2015-05-14 01:40:28 -0700

    Removed seed data and added poi name extractor alg

--- app/helpers/pois_helper.rb
+++ app/helpers/pois_helper.rb
@@ -15,13 +15,26 @@ module PoisHelper
       geocode_response = geocode_converter(hash["value"])
       hash_coordinates = geocode_response["geometry"]["location"]
       hash_place_id = geocode_response["place_id"]
+      poiName = address_name_getter(hash["value"])
       Poi.create(
         trip_id: params[:trip_id],
+        poi_name: poiName,
         address: hash["value"],
         geocode_longitude: hash_coordinates["lng"],
         geocode_latitude: hash_coordinates["lat"],
         google_place_id: hash_place_id        
         )
     end
+  end
+
+  def address_name_getter(input)
+    full_address = input
+    result = []
+    full_address_array = full_address.split("")
+    comma_index = full_address_array.find_index(",")-1
+    full_address_array.each_index do |e|
+      result.push(full_address_array[e]) if e <= comma_index
+    end
+    result.join()
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,25 @@ User.create(
   password_digest: "will"
   )
 
-5.times do
-  Trip.create(
-    user_id: 1,
-    trip_name: Faker::App.name,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    )
-end
+# 5.times do
+#   Trip.create(
+#     user_id: 1,
+#     trip_name: Faker::App.name,
+#     city: Faker::Address.city,
+#     state: Faker::Address.state,
+#     )
+# end
 
 
-25.times do
-  Poi.create(
-    trip_id: rand(1..5),
-    poi_name: Faker::App.name,
-    address: Faker::Address.street_address,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    zip: Faker::Address.zip
-    )
-end
+# 25.times do
+#   Poi.create(
+#     trip_id: rand(1..5),
+#     poi_name: Faker::App.name,
+#     address: Faker::Address.street_address,
+#     city: Faker::Address.city,
+#     state: Faker::Address.state,
+#     zip: Faker::Address.zip
+#     )
+# end
 
 
